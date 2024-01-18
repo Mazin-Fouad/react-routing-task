670c25095b56ab8c3eda6121d31107671ff4d42b
Mazin Fouad <m.fouad@live.de>
2024-01-18 11:01:54 +0100

    not found page style modified

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -5,14 +5,10 @@ import '../mainPage/mainPage.css'; // same html structure & css as in mainPage
 
 class About extends Component {
     state = {  } 
-    /**
-     * Renders the component and returns the JSX markup.
-     *
-     * @return {JSX.Element} The JSX markup for the component.
-     */
+  
     render() { 
         return (<div>
-            <h1 className='tracking-in-contract'>Hello binary<span className='bold'>:</span>State team from About Page <span>&gt;_</span></h1>
+            <h1 className='tracking-in-contract'>Hello binary<span className='bold'>:</span>state team from About Page <span>&gt;_</span></h1>
             <Link className='link' to="/">Back to main page</Link>
 
         </div>);
--- src/components/mainPage/mainPage.jsx
+++ src/components/mainPage/mainPage.jsx
@@ -5,14 +5,9 @@ import { Link } from 'react-router-dom';
 
 class MainPage extends Component {
     state = {  } 
-    /**
-     * Render the component.
-     *
-     * @return {JSX.Element} The rendered component.
-     */
     render() { 
         return (<div>
-            <h1 className='tracking-in-contract'>Hello binary<span className='bold'>:</span>State team from Main Page <span>&gt;_</span></h1>
+            <h1 className='tracking-in-contract'>Hello binary<span className='bold'>:</span>state team from Main Page <span>&gt;_</span></h1>
             <Link className='link' to="/about">Go to about us page</Link>
 
         </div>);
